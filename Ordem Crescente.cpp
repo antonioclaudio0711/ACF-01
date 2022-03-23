4014f428703a6227f15baaf86bab14b8d9352ac0
Antônio Claudio Ferreira Filho <95254894+antonioclaudio0711@users.noreply.github.com>
2022-03-23 09:06:49 -0300

    Rename Programa Ordem Crescente.cpp to Ordem Crescente.cpp

--- Ordem Crescente.cpp
+++ Ordem Crescente.cpp
@@ -15,32 +15,32 @@ main()
 	scanf("%f",&numero03);
 	if (numero01 < numero02 && numero01 < numero03 && numero02 < numero03)
 	{
-		printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero01, numero02, numero03);
+		printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero01, numero02, numero03);
 		printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero01, numero02, numero01, numero03, numero02, numero03);
 	}
 		else if (numero01 < numero02 && numero01 < numero03 && numero03 < numero02)
 		{
-			printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero01, numero03, numero02);
+			printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero01, numero03, numero02);
 			printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero01, numero02, numero01, numero03, numero03, numero02);
 		}
 			else if (numero02 < numero01 && numero02 < numero03 && numero01 < numero03)
 			{	
-				printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero02, numero01, numero03);
+				printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero02, numero01, numero03);
 				printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero02, numero01, numero02, numero03, numero01, numero03);
 			}
 				else if (numero02 < numero01 && numero02 < numero03 && numero03 < numero01)
 				{		
-					printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero02, numero03, numero01);
+					printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero02, numero03, numero01);
 					printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero02, numero01, numero02, numero03, numero03, numero01);
 				}
 					else if (numero03 < numero01 && numero03 < numero02 && numero01 < numero02)
 					{	
-						printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero03, numero01, numero02);
+						printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero03, numero01, numero02);
 						printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero03, numero01, numero03, numero02, numero01, numero02);
 					}
 						else if (numero03 < numero01 && numero03 < numero02 && numero02 < numero01)
 						{			
-							printf("A ordem crescente dos números inseridos é: %.2f; %.2f; %.2f.\n", numero03, numero02, numero01);
+							printf("A ordem crescente dos nÃºmeros inseridos Ã©: %.2f; %.2f; %.2f.\n", numero03, numero02, numero01);
 							printf("%.2f < %.2f e %.2f < %.2f e %.2f < %.2f.", numero03, numero01, numero03, numero02, numero02, numero01);
 						}
 }
