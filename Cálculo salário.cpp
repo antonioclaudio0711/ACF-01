ecea950715cfdf317b5bc37225140bc4ebeb2d22
Antônio Claudio Ferreira Filho <95254894+antonioclaudio0711@users.noreply.github.com>
2022-03-23 09:04:42 -0300

    Rename Programa Salário.cpp to Cálculo salário.cpp

--- Cálculo salário.cpp
+++ Cálculo salário.cpp
@@ -8,19 +8,19 @@ main()
 	int horas_de_trabalho_mes, hora_extra;
 	float salario_hora, salario_total;
 	
-	printf("Insira a quantidade de horas trabalhadas durante o mês:");
+	printf("Insira a quantidade de horas trabalhadas durante o mÃªs:");
 	scanf("%i",&horas_de_trabalho_mes);
-	printf("Insira o salário recebido por hora de trabalho:");
+	printf("Insira o salÃ¡rio recebido por hora de trabalho:");
 	scanf("%f",&salario_hora);
 	if (horas_de_trabalho_mes <= 160)
 	{
 		salario_total = salario_hora * horas_de_trabalho_mes;
-		printf("Diante de %i horas trabalhadas no mês, o salário recebido será de: %.2f.", horas_de_trabalho_mes, salario_total);
+		printf("Diante de %i horas trabalhadas no mÃªs, o salÃ¡rio recebido serÃ¡ de: %.2f.", horas_de_trabalho_mes, salario_total);
 	}
 		else if (horas_de_trabalho_mes > 160)
 		{
 			hora_extra = horas_de_trabalho_mes - 160;
 			salario_total = (160 * salario_hora) + (1.5 * salario_hora * hora_extra);
-			printf("Diante de %i horas trabalhadas no mês, o salário recebido será de: %.2f.", horas_de_trabalho_mes, salario_total);
+			printf("Diante de %i horas trabalhadas no mÃªs, o salÃ¡rio recebido serÃ¡ de: %.2f.", horas_de_trabalho_mes, salario_total);
 		}
 }
